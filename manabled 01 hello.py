d6dddbe8bbdb2d8d52b1af7577d9f3af803018c0
manabled <31279071+manabled@users.noreply.github.com>
2017-08-30 19:52:06 -0400

    first assignment

---
+++ manabled 01 hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
+
+print('Hello, Class!')
+
+print('My name is Mason Bledsoe.')
+
+print('My favorite game is Fable')
+
+print('I have no concerns about this class at the moment, aside from how early it is.')
+
+print('I am excited to learn how to design games.')
+
+print('My StackExchange number is 8506195')
+
+print('the URL to my github account is https://github.com/manabled.')
